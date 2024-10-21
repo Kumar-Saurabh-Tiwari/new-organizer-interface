d9b16cdd526de7078a9d29527d85a42cb8a83dc0
Kumar-Saurabh-Tiwari <tiwarisaurabhkumar0@gmail.com>
2024-10-21 19:52:18 +0530

    code updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { EventDashboardComponent } from './component/event-dashboard/event-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'events', component: EventDashboardComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
